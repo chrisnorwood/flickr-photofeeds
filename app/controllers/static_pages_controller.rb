f5116a4dae329163c132dd3c7f94a5ee1b3f3223
Chris Norwood <cnorwood92@gmail.com>
2016-10-15 19:32:14 -0700

    Fix problem with search bar

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController
   def index
-    @user_id = (params[:user_id].nil? || params[:user_id].empty?) ? '67741638@N06' : params[:user_id]
+    if params[:user_id].nil? || params[:user_id] == ''
+      @user_id = '67741638@N06'
+    else
+      @user_id = params[:user_id]
+    end
   end
 end
